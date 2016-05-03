cda8d9fe0c380a948f90e47f1cce140b6175a499
Vagelis Kostopoulos <kostopoulos.e@gmail.com>
2016-05-03 16:57:51 +0100

    Welcome from feature 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Hi Laravel 5 from feature 1</div>
             </div>
         </div>
     </body>
